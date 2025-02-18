0ae89e4bd282fe9dd58a841c841a317b282f8ce7
Matheus Lopes <matheuslopes.developer@gmail.com>
2025-02-18 17:19:40 -0300

    feat: add last_interaction_at column to carts table for tracking user activity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_16_193209) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_18_201910) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_16_193209) do
     t.decimal "total_price", precision: 17, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "last_interaction_at"
   end
 
   create_table "products", force: :cascade do |t|
